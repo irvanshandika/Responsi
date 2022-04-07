fea0fa0e408c40e7781bbd01774d7a05ef421cf8
irvanshandika <shandikamuhammadirvan@gmail.com>
2022-04-07 21:02:39 +0700

    LatihanOverloadingMethod

---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+
+        }
+    }
+}
