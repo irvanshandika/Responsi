811f8a07b218e3163b9d6d4688060b6de84cfb0f
irvanshandika <shandikamuhammadirvan@gmail.com>
2022-04-19 16:35:28 +0700

    Responsi

---
+++ ResponsiPemrograman4285/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ResponsiPemrograman4285
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+
+            Console.Write("Nama Karyawan : ");
+            karyawan.Nama = Console.ReadLine();
+
+            Console.Write("NIK : ");
+            karyawan.Nik = Console.ReadLine();
+
+            Console.Write("Gaji Bulanan : ");
+            karyawan.GajiBulanan = Console.ReadLine();
+
+            Karyawan karyawan2 = new Karyawan()
+
+
+        }
+    }
+}
